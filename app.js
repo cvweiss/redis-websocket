0a8622462acd3eb394ea9f41dcc4b5ec92cf145c
Clinton Weiss <cvweiss@gmail.com>
2018-06-06 00:45:29 +0000

    allows subscription to any redis channel custom to each connection

--- app.js
+++ app.js
@@ -8,8 +8,29 @@ const http = require("http").createServer((req, res) => { res.writeHead(404); re
 const ws = new (require("websocket").server)({ httpServer: http, autoAcceptConnections: true });
 
 console.log(`${Date()} Server started and listening on port ${port}`);
-redis.on("message", (channel, message) => {
-        ws.connections.forEach(connection => connection.send(message));
-        console.log(`${Date()} Broadcasted to ${ws.connections.length} clients: ${message}`);
+redis.on("pmessage", (pattern, channel, message) => {
+        var count = 0;
+        ws.connections.forEach( (connection) => {
+            if (connection.subscriptions instanceof Array) {
+                if (connection.subscriptions.indexOf(channel) != -1) {
+                    connection.send(message);
+                    count++;
+                }
+            }
         });
-redis.subscribe("public");
+        console.log(`${Date()} Broadcasted to ${count} clients: ${message}`);
+        });
+redis.psubscribe("*");
+
+ws.on('connect', (connection) => {
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            try {
+                var data = JSON.parse(message.utf8Data);
+                if (connection.subscriptions === undefined) connection.subscriptions = new Array();
+                if (data.action == 'sub') connection.subscriptions.push(data.channel);
+            } catch (e) {
+            };
+        }
+    });    
+});
